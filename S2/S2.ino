05ecf0e73da4f00f669440ca4397b977990f6b8a
Leonardo Martins <leonardo_m_brito@estudante.sesisenai.org.br>
2025-11-24 12:39:29 -0300

    add s2

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,71 +1,155 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
-WiFiClient client;
-PubSubClient mqtt(client);
 
-const String SSID = "FIESC_IOT_EDU";
-const String PASS = "8120gv08";
+const char* SSID = "FIESC_IOT_EDU";
+const char* PASS = "8120gv08";
 
-const int PORT = 1883;
-const String URL = "test.mosquitto.org";
-const String broker_user = "";
-const String broker_pass = "";
 
-const String MyTopic = "mat_brum";
-const String OtherTopic = "leo_pauli";
+const char* BROKER = "e6607d8d5fec40a9974cfc1552a13e2f.s1.eu.hivemq.cloud";
+const int PORT = 8883;
 
-const int LED_PIN = 2;
+const char* BROKER_USER = "leonardo_martins";
+const char* BROKER_PASS = "Integrante4";
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+#define TRIG1 12
+#define ECHO1 25
+#define TRIG2 18
+#define ECHO2 21
+#define PINO_LED 4
 
-  WiFi.begin(SSID.c_str(), PASS.c_str());
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(200);
-  }
-  Serial.println("\nConectado ao WiFi!");
+// + e -  -> 3v3 e GND da placa
 
-  mqtt.setServer(URL.c_str(), PORT);
-  mqtt.setCallback(callback);
-  while (!mqtt.connected()) {
-    String ID = "S1-" + String(random(0xffff), HEX);
-    if (mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str())) {
-      Serial.println("\nConectado ao broker!");
-      mqtt.subscribe(MyTopic.c_str());
-    } else {
-      Serial.print(".");
-      delay(200);
-    }
-  }
+
+const char* TOPICO_PUB_DIST1 = "S2/Distancia1";
+const char* TOPICO_PUB_DIST2 = "S2/Distancia2";
+const char* TOPICO_PUB_ILU   = "S2/Cmd";
+const char* TOPICO_ENVIO_S3  = "S3/Cmd";
+
+//iluminacao     
+
+
+WiFiClientSecure espClient;
+PubSubClient mqtt(espClient);
+
+
+long medirDistancia(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duracao = pulseIn(echoPin, HIGH, 30000);
+  long distancia = duracao * 0.034 / 2;
+  return distancia;
 }
 
-void loop() {
-  mqtt.loop();
-  if (Serial.available() > 0) {
-    String mensagem = "Mateus: " + Serial.readStringUntil('\n');
-    mqtt.publish(OtherTopic.c_str(), mensagem.c_str());
-  }
-  delay(2000);
-}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String mensagem = "";
+
   for (int i = 0; i < length; i++) {
     mensagem += (char)payload[i];
   }
 
-  Serial.print("Recebido: ");
+  Serial.print("[CMD] ");
   Serial.println(mensagem);
 
-  if (mensagem == "LED_ON") {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED aceso!");
-  } else if (mensagem == "LED_OFF") {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED apagado!");
+  // Comandos recebidos do tópico S2/Cmd
+  if (mensagem == "acender") {
+    digitalWrite(PINO_LED, HIGH);
+  } 
+  else if (mensagem == "apagar") {
+    digitalWrite(PINO_LED, LOW);
   }
+}
+
+
+void conectaWiFi() {
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi conectado!");
+}
+
+
+void conectaMQTT() {
+  mqtt.setServer(BROKER, PORT);
+  mqtt.setCallback(callback);
+  espClient.setInsecure();
+
+  while (!mqtt.connected()) {
+    String clientId = "S2_" + String(random(0xffff), HEX);
+
+    if (mqtt.connect(clientId.c_str(), BROKER_USER, BROKER_PASS)) {
+      mqtt.subscribe(TOPICO_PUB_ILU);
+      //todos os tópicos para se inscrever
+    } else {
+      delay(1000);
+      Serial.print('.');
+    }
+  }
+  Serial.println("MQTT conectado!");
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  espClient.setInsecure();
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO2, INPUT);
+
+  pinMode(PINO_LED, OUTPUT);
+  digitalWrite(PINO_LED, LOW);
+
+  conectaWiFi();
+  conectaMQTT();
+}
+
+
+void loop() {
+if (!mqtt.connected()) {
+    conectaMQTT();
+  }
+
+  // --- Ler ultrassonico 1 ---
+  long duracao1;
+  long distancia1;
+
+  digitalWrite(TRIG1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1, LOW);
+
+  duracao1 = pulseIn(ECHO1, HIGH);
+  distancia1 = duracao1 * 0.034 / 2; 
+
+  if (distancia1 < 10) {
+    mqtt.publish(TOPICO_PUB_DIST1, "Detectado");
+  }
+
+
+  long duracao2;
+  long distancia2;
+
+  digitalWrite(TRIG2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG2, LOW);
+
+  duracao2 = pulseIn(ECHO2, HIGH);
+  distancia2 = duracao2 * 0.034 / 2;
+
+  if (distancia2 < 10) {
+    mqtt.publish(TOPICO_PUB_DIST2, "Detectado");
+  }
+  mqtt.loop();
 }
