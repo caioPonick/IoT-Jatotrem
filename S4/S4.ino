8e9c1dad3ff694f103a12eca819bbd939dcdb45e
Mateusbrum01 <mateus_cagneti@estudante.sesisenai.org.br>
2025-11-24 12:52:14 -0300

    add s4

---
+++ S4/S4.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+c:\Users\mateus_cagneti\Documents\IoT-Jatotrem\S4\S4.ino
